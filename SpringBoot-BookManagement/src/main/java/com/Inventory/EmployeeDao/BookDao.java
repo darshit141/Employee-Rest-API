c02a3953ca9a401f1ce8a5957c619edf226fe1e8
darshit141 <93430931+darshit141@users.noreply.github.com>
2022-10-03 09:18:52 +0530

    Delete SpringBoot-BookManagement directory

--- SpringBoot-BookManagement/src/main/java/com/Inventory/EmployeeDao/BookDao.java
+++
@@ -1,9 +0,0 @@
-package com.Inventory.EmployeeDao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import com.Inventory.Entity.Book;
-
-public interface BookDao extends JpaRepository<Book,Integer> {
-
-}
